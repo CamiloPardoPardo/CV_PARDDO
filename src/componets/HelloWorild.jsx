2d978163af7c0c2cffd96f1d55bc2176cb459e0b
Camilo Pardo <camilopardochoachi@gmail.com>
2020-12-16 22:40:57 -0500

    Clase numero 12, implementación de sass

--- src/componets/HelloWorild.jsx
+++ src/componets/HelloWorild.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import '../assets/styles/app.scss'
 const HelloWorld= () => (
     <h1>Hola Alex</h1>
     
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const HtmlWebPackPlugin= require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -25,6 +28,16 @@ module.exports = {
                         loader: "html-loader"
                     }
                 ]
+            },
+            {
+                test: /\.(s*)css$/,
+                use:[
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
     },
@@ -33,6 +46,9 @@ module.exports = {
             template: './public/index.html',
             filename: './index.html'
 
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].css'
         }),
     ]
 };
